c59874a27c0781c7d7d6fd6918a37389c99a513b
owkmear <h3s8qp9xw3f@gmail.com>
2023-03-29 17:53:40 +0300

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { NewsController } from './news.controller';
 import { AppService } from './app.service';
 import { NewsService } from './news.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    load: [configuration]
+  })],
   controllers: [AppController, NewsController],
   providers: [AppService, NewsService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-const PORT = 3007;
+import configuration from './config/configuration';
 
 async function bootstrap() {
+  const { port } = configuration();
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(port);
 }
 bootstrap();
